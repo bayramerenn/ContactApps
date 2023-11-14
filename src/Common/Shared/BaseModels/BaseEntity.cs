ba0165421d611b4cc287f585f90b96b9d16fbcc3
Bayram Eren <bayram.eren@outlook.com.tr>
2023-11-14 23:47:11 +0300

    The dependencies of the layers have been configured. In the Shared layer, BaseModel, Exception, and Middleware have been created.

---
+++ src/Common/Shared/BaseModels/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.BaseModels
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ src/Common/Shared/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Shared.BaseModels;
+using Shared.Exceptions;
+using System.Diagnostics;
+using System.Net;
+using ValidationException = Shared.Exceptions.ValidationException;
+
+namespace Shared.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+                _logger.LogError(ex, ex.Message);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            HttpStatusCode code = HttpStatusCode.InternalServerError;
+            string? traceId = Activity.Current?.TraceId.ToString();
+            string message = exception.Message;
+
+            if (exception is ValidationException validation)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+
+                await context.Response.WriteAsync(new ErrorDetails
+                {
+                    TraceId = traceId,
+                    Message = message,
+                    Errors = validation.Errors
+                }.ToString());
+            }
+            else
+            {
+                if (exception is DuplicateException)
+                {
+                    code = HttpStatusCode.Conflict;
+                }
+                else if (exception is ArgumentException)
+                {
+                    code = HttpStatusCode.NotFound;
+                }
+                else if (exception is NotFoundException)
+                {
+                    code = HttpStatusCode.NotFound;
+                }
+                else if (exception is BadRequestException)
+                {
+                    code = HttpStatusCode.BadRequest;
+                }
+
+                context.Response.StatusCode = (int)code;
+
+                await context.Response.WriteAsync(new ErrorDetails
+                {
+                    TraceId = traceId,
+                    Message = message
+                }.ToString());
+            }
+        }
+    }
+}
