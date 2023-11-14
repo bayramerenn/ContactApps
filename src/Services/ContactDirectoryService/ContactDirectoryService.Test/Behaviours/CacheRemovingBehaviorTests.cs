a7ae33ba1bdd472bc4add7746f9c753b83af548d
Bayram Eren <bayram.eren@outlook.com.tr>
2023-11-15 01:00:57 +0300

    Tests for cache behaviors have been written.

---
+++ src/Services/ContactDirectoryService/ContactDirectoryService.Test/Behaviours/CacheRemovingBehaviorTests.cs
@@ -0,0 +1,29 @@
+﻿using ContactDirectoryService.Application.Common.Behaviours.Caching;
+using ContactDirectoryService.Application.Features.ContactInformations.Command;
+using Moq;
+using Shared.CacheService;
+
+namespace ContactDirectoryService.Test.Behaviours
+{
+    public class CacheRemovingBehaviorTests : BaseTestFixture
+    {
+        [Test]
+        public async Task Handle_Should_Remove_Cache()
+        {
+            var cacheMock = new Mock<IRedisCache>();
+
+            var cachingBehavior = new CacheRemovingBehavior<CreateContactInformationCommand, Guid>(cacheMock.Object);
+
+            var query = new CreateContactInformationCommand(Guid.NewGuid(), Domain.Enums.ContactType.Phone, "1234");
+
+            cacheMock.Setup(x => x.RemovePatternAsync(It.IsAny<string>(), 0))
+                .Returns(Task.CompletedTask);
+
+            await cachingBehavior.Handle(query,
+                () => Task.FromResult(Guid.NewGuid()),
+                CancellationToken.None);
+
+            cacheMock.Verify(x => x.RemovePatternAsync(query.CacheKey, 0), Times.Once);
+        }
+    }
+}
---
+++ src/Services/ContactDirectoryService/ContactDirectoryService.Test/Behaviours/CachingBehaviorTests.cs
@@ -0,0 +1,35 @@
+﻿using ContactDirectoryService.Application.Common.Behaviours.Caching;
+using ContactDirectoryService.Application.Features.ContactInformations.Queries;
+using Microsoft.Extensions.Options;
+using Moq;
+using Shared.CacheService;
+
+namespace ContactDirectoryService.Test.Behaviours
+{
+    public class CachingBehaviorTests : BaseTestFixture
+    {
+        [Test]
+        public async Task Handle_Should_Return_Cached_Response()
+        {
+            var cacheMock = new Mock<IRedisCache>();
+            var cacheSettings = new CacheSettings { SlidingExpiration = 300 }; // Set a sliding expiration time for testing
+
+            var cachingBehavior = new CachingBehavior<GetLocationReportQuery, IEnumerable<GetLocationReportResponse>>(cacheMock.Object, Options.Create(cacheSettings));
+            var query = new GetLocationReportQuery(Guid.NewGuid());
+
+            var cachedResponse = new List<GetLocationReportResponse> { new GetLocationReportResponse() };
+
+            cacheMock.Setup(x => x.GetAsync<IEnumerable<GetLocationReportResponse>>(It.IsAny<string>(), 0))
+                .ReturnsAsync(cachedResponse);
+
+            var result = await cachingBehavior.Handle(query,
+                  () => Task.FromResult<IEnumerable<GetLocationReportResponse>>(cachedResponse.ToList()),
+                  CancellationToken.None);
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<IEnumerable<GetLocationReportResponse>>();
+
+            cacheMock.Verify(x => x.GetAsync<IEnumerable<GetLocationReportResponse>>(query.CacheKey, 0), Times.Once);
+        }
+    }
+}
