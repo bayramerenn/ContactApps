4d08baf36c8491579f229ccbd9d4c057781c0b5b
Bayram Eren <bayram.eren@outlook.com.tr>
2023-11-15 00:24:53 +0300

    The create operation for the Contact object has been implemented.

---
+++ src/Services/ContactDirectoryService/ContactDirectoryService.API/Controllers/ContactsController.cs
@@ -0,0 +1,14 @@
+﻿using ContactDirectoryService.Application.Features.Contract.Commands.CreateContact;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactDirectoryService.API.Controllers
+{
+    public class ContactsController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateContactCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await Sender.Send(command, cancellationToken));
+        }
+    }
+}
